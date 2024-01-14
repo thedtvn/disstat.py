c0aec2c7a0c5f5cd552debd475fe44c5fc185049
The DT <80153297+thedtvn@users.noreply.github.com>
2024-01-14 09:42:50 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ version = ''
 with open('./disstat/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
+requirements = []
+with open('./req.txt') as f:
+    requirements = f.read().splitlines()
+
+
 setup(name='disstat.py',
       version=version,
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
       license='Apache License 2.0',
       packages=['disstat'],
-      install_requires=[
-            "aiohttp~=3.8.6",
-            "psutil~=5.9.6",
-            "setuptools~=65.5.0",
-            "discord"
-      ],
+      install_requires=requirements,
       author='The DT',
       url='https://github.com/thedtvn/disstat.py',
       description='disstat.py is a python library for DisStat'
